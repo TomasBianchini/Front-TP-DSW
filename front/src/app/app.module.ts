099e4c12694639f297742d537a7be5308828a758
Tomas Bianchini <tbianchini@frro.utn.edu.ar>
2024-02-21 09:53:46 -0300

    adding components and services

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategoryComponent } from './components/category-components/category/category.component';
+import { DiscountComponent } from './components/discount-components/discount/discount.component';
+import { PaymentTypeComponent } from './components/payment-type-components/payment-type/payment-type.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryComponent,
+    DiscountComponent,
+    PaymentTypeComponent
   ],
   imports: [
     BrowserModule,
---
+++ front/src/app/components/payment_type-components/payment-type/payment-type.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-type',
+  templateUrl: './payment-type.component.html',
+  styleUrls: ['./payment-type.component.css']
+})
+export class PaymentTypeComponent {
+
+}
